48da5d9b31425ee236d1f1cc166d1437e10fbca0
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 09:53:20 -0700

    add cin and namespace

--- Module1/LA1-1/main.cpp
+++ Module1/LA1-1/main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
+using namespace std;
 
 int main()
 {
-        std::cout << "Now inside VSCode and github" << std::endl;
-        return 0;
+    int age;    // int: signed integers
+    // cout: for system output
+    std::cout << "Now inside VSCode and github" << std::endl;
+    cout << "Another line under namespace std" << std::endl;
+    cout << "Wildcats"<<" "<<"WSU "<<"Ogden " << std::endl;
+    // cin: for system/user input
+    cout << "How old are you?" << endl;
+    cin >> age; 
+    cout << "I see you are: " << age << endl;
+    return 0;
 }
