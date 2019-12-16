7ee3b4e5497a53c3fdd8917871e2aaa65902296a
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-16 10:37:50 -0700

    Initial file

---
+++ Module1/LA1-1/variables.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "\nData Type Bytes"
+        << "\n---------  ----"
+        << "\nint          " << sizeof(int)    // integer
+        << "\nchar         " << sizeof(char)   // characters 1 by default
+        << "\nlong         " << sizeof(long)   // long integers
+        << "\nbool         " << sizeof(bool)   // boolean
+        << endl;
+    return 0;
+}
+
