85fddfe3b5234f92cecc0c78208162bd17fb98eb
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 10:22:30 -0700

    Test and compiling

--- Module3/TO3-1/src/distance.cpp
+++ Module3/TO3-1/src/distance.cpp
@@ -5,3 +5,59 @@
 #include <cmath> 
 using namespace std;
 #include "distance.h"
+
+void Distance::ShowDist() const
+{
+    std::cout << feet() << "\'-" << inches() << "\"" << std::endl;
+}
+
+Distance Distance::operator+ (Distance rhs) const
+{
+    // Distance temp;
+    int feet = feet_ + rhs.feet_;
+    float inches = inches_ + rhs.inches_;
+    // Update values IF inches > 12
+    if(inches >= 12.0)
+    {
+        inches -= 12.0;
+        feet++;
+    }
+    // Distance temp(feet, inches);
+    // return temp;
+    return Distance(feet, inches);
+}
+
+Distance Distance::operator -(Distance rhs) const
+{
+    // Distance temp;
+    int ft = feet_ - rhs.feet_;
+    float in = 0;
+    // Update values IF inches < 12
+    if(inches_ < 12.0)
+    {
+        ft -= 1;
+        in = (inches_ + 12) - rhs.inches_;
+    }
+    // Update values IF inches > 12
+    if(in >= 12.0)
+    {
+        in -= 12.0;
+        ft++;
+    }
+    // Distance temp(feet, inches);
+    // return temp;
+    return Distance(ft, in);
+}
+
+void Distance ::update_distance(int ft, float in)
+{
+    set_feet(ft);
+    set_inches(in);
+}
+
+   std::ostream&operator << (std::ostream &os, Distance& distance)
+   {
+       // recommended not to include a '\n' or endl character
+       os << "feet: " << distance.feet_ << " inches: " << distance.inches_;
+       return os;
+   }
--- Module3/TO3-1/src/main.cpp
+++ Module3/TO3-1/src/main.cpp
@@ -5,5 +5,26 @@ using namespace std;
 // Main Function
 int main() 
 {
+  Distance d1(45,9.5);
+  d1.ShowDist();
+
+  Distance d2(10,8.5);
+  d2.ShowDist();
+
+  Distance d3;
+  d3 = d1 + d2;
+  d3.ShowDist();
+
+  Distance d4;
+  d4 = d1 + d2 + d3;
+  d4.ShowDist();
+
+  // display info with stdout
+  std::cout << d4 << std::endl;
+
+  d3 = d1 - d2;
+  d3.ShowDist();
+
   return 0;
 }
+
