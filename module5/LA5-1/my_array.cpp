f1e377bd6d6f18aab2732b9dc57da6de0e8b5c82
CCEClass1 <CCEClass1@ad.weber.edu>
2019-12-19 14:15:43 -0700

    various for loops

--- module5/LA5-1/my_array.cpp
+++ module5/LA5-1/my_array.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
-using namespace std;
+// using namespace std;
 #include <array>
-#include <algorithm>
+#include <algorithm> // for iterators
 
 // Local Prototypes
 void fun(int const& value); 
@@ -9,6 +9,45 @@ void fun(int const& value);
 
 int main()
 {
+    std::array<int, 3> info = {4, 7, 9};
+    std::cout<<"First Element: "<<info.front() << std::endl;
+    std::cout<<"Last Element: "<<info.back() << std::endl;
+    std::cout<<"Size of Array: "<<info.size() << std::endl;
+
+    // Iterate over elements
+    std::cout<<"Loop using index notation: " << std::endl;
+    for (size_t i = 0; i < info.size(); i++)
+    {
+        std::cout<<&info[i]<< " " <<info[i]<<std::endl;   
+    }
+
+    // Iterate using iterators
+    std::cout<<"Loop using iterator notation. Using built-in functions for" << " begin and end members of containers" << std::endl;
+    for (auto it = std::begin(info); it != std::end(info); ++it)
+    {
+        // std::cout<<it<<std::endl; // "it" is a pointer (address)
+        std::cout<<it << " "<< *it <<std::endl; // "*it": gets the value at the address
+
+    }
+    // Range based loops
+    std::cout<<"Range based loops"<<std::endl;
+    for(const auto& value: info)
+    {
+        std::cout<<value<<std::endl;
+    }
+
+    // "For Each" loops
+    // functor: functions that act like objects
+    std::cout<<"Using for_each algorithm, using a function or \"functor\" loops"<<std::endl;
+    std::for_each(std::begin(info), std::end(info), fun);
+    
+    // "For Each" loops using lambda: [](){}   <--- Order of operations
+    // [] to take any local variables in the scope
+    // () declare the parameters of your functions
+    // {} declare the body of your function
+    std::cout<<"Using for_each algorithm, using lambdas"<<std::endl;
+    std::for_each(std::begin(info), std::end(info), [](int const& value){std::cout<<value<<"\n";});
+
     return 0;
 }
 
--- module5/LA5-1/my_structures.cpp
+++ module5/LA5-1/my_structures.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
 using namespace std;
 
+struct Part
+{
+   int model_number;
+   int part_number;
+   float cost;
+};
+
+struct Car
+{
+   Part doors;
+   Part windows;
+   Part tires;
+};
+
+
 // Local Prototypes
 
 int main()
 {
+   Part part1;
+   part1.model_number = 6244;
+   part1.part_number = 373;
+   part1.cost = 45.90;
+   std::cout << "P1 Model: " << part1.model_number << " Part: " << part1.part_number << " Cost: $" << part1.cost << std::endl;
+   
+   Part part2 = {6277, 781, 32.10}; // declare and initialize
+   std::cout << "P2 Model: " << part2.model_number << " Part: " << part2.part_number << " Cost: $" << part2.cost << std::endl;
+
+   // Part part3;
+   // part3 = part2;
+   // std::cout << "P3 Model: " << part3.model_number << " Part: " << part3.part_number << " Cost: $" << part3.cost << std::endl;
+   Car sedan;
+   sedan.doors.part_number = 101;
+   sedan.windows.model_number = 7002;
+   sedan.windows.cost = 89.99;
+
+   
+   
    return 0;
 }
